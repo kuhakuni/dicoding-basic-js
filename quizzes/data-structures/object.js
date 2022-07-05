3042e4beeae2e2983772cd2ca10a4192cf5d8bf2
kuhakuni <afif.haidar@gmail.com>
2022-07-05 11:46:34 +0700

    add data structures quizzes

---
+++ quizzes/data-structures/object.js
@@ -0,0 +1,39 @@
+/**
+ * TODO
+ * 1. Buatlah variabel dengan nama restaurant yang bertipe object dengan ketentuan berikut:
+ *    - Memiliki properti bernama "name"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti bernama "city"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "favorite drink"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "favorite food"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "isVegan"
+ *       - Bertipe data boolean
+ *       - Bernilai boolean apa pun.
+ *
+ * 2. Buatlah variabel bernama name.
+ *    Kemudian isi dengan nilai name dari properti object restaurant
+ * 3. Buatlah variabel bernama favoriteDrink.
+ *    Kemudian isi dengan nilai "favorite drink" dari properti object restaurant
+ */
+
+// TODO
+let restaurant = {
+	name: "Mc Donalds",
+	city: "Malang",
+	"favorite drink": "Lemon Tea",
+	"favorite food": "Big Mac",
+	isVegan: false,
+};
+let favoriteDrink = restaurant["favorite drink"];
+let { name } = restaurant;
+/**
+ * Jangan hapus kode di bawah ini
+ */
+module.exports = { restaurant, name, favoriteDrink };
